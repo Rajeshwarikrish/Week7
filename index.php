caa23dbecfc498de8d7fe35338a19001f085902f
rajeshwari krishnamurthy <rk633@afsconnect1.njit.edu>
2017-11-03 00:45:33 -0400

    Code to create table added

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ try
 {
    $conn = new PDO("mysql:host=$hostname;dbname=rk633",$username, $password);
    echo "Connection Successful<br>";
+   runQuery();
 }
 catch(PDOException $e)
 {
@@ -14,15 +15,16 @@ catch(PDOException $e)
    $e->getMessage());
 }
 
-function runQuery($query) {
-   gobal $conn;
+function runQuery() {
+   global $conn;
      try {
-      	    $q = Select* from accounts where id<6;
+      	    $query = "Select * from accounts where id<6";
 	    $q = $conn->prepare($query);
 	    $q->execute();
 	    $results = $q->fetchAll();
 	    $q->closeCursor();
-	    return $results;
+	    //return $results;
+	    print_r($results);
 	  }
      catch (PDOException $e) {
      	    http_error("500 Internal Server Error\n\n"."There was a SQL
@@ -34,5 +36,17 @@ function http_error($message)
 {
 	header("Content-type: text/plain");
 	die($message);
-}       
+}      
+function htmltable()
+{
+	$data = runQuery();
+	echo "<table>";
+	foreach ($rows as $row) {
+	   echo "<tr>";
+	   foreach ($row as $column) {
+	      echo "<td>$columns</td>";
+	   }
+	   echo "</tr>";
+	}
+	echo "</table>";
 ?>
