dc1181d1dddeb4f5a47d0e0cb504f633caee8468
rajeshwari krishnamurthy <rk633@afsconnect1.njit.edu>
2017-11-05 15:17:16 -0500

    Code modified to display header from database

--- index.php
+++ index.php
@@ -11,7 +11,9 @@ try
    $res_query = runQuery($query);
    $no_of_rows = count($res_query);
    echo $no_of_rows . ' no of records have id less than 6';
-   echo htmltable($res_query);
+   $str = "SHOW COLUMNS FROM accounts"; 
+   $heading = runHeading($str);
+   echo htmltable($heading, $res_query);
 }
 catch(PDOException $e)
 {
@@ -29,38 +31,35 @@ function runQuery($query) {
 	    return $results;
 	  }
      catch (PDOException $e) {
-     	    http_error("500 Internal Server Error\n\n"."There was a SQL
-	    error:\n\n" . $e->getMessage());
+     	    http_error("500 Internal Server Error\n\n"."There was a SQL error:\n\n" . $e->getMessage());
      }
 }
 
-function http_error($message)
-{
-	header("Content-type: text/plain");
-	die($message);
-}      
-function htmltable($res_query)
-{	//print_r($res_query);
-	//echo "<br>";
+function runHeading($query)  {
+  global $conn;
+    try {
+            $q = $conn->prepare($query);
+	    $q->execute();
+	    $results = $q->fetchAll(PDO::FETCH_COLUMN);
+	    $q->closeCursor();
+	    return $results;
+	}
+     catch (PDOException $e) {
+           http_error("500 Internal Server Error\n\n"."There was a SQL error:\n\n" . $e->getMessage());
+     }
+}
+     
+function htmltable($heading,$res_query)
+{	
 	$table = NULL;
 	$table .= "<table border = 1>";
-	/*$field_num = mysql_num_fields($query);
-	echo "<thead>";
-	for ($i=0; $i<$field_num; $i++)
-	{
-	$field = mysqli_fetch_field($query);
-	echo "<th>{$field->name}</th>";
-	}*/
-	echo '<table class="data-table"><tr class = "data-heading">';
-	while($property = mysqli_fetch_field($query)){
-	echo '<td>' . $property->name . '</td>';
-	array_push($all_property, $property->name);
+	foreach ($heading as $head)  {
+	   $table .= "<th>" . $head . "</th>";
 	}
-	echo '</tr>';
 	foreach ($res_query as $row) {
 	   $table .= "<tr>";
 	   foreach ($row as $column) {
-	      $table .= "<td>$column</td>";
+	      $table .= "<td>" . $column ."</td>";
 	   }
 	   $table .="</tr>";
 	}
